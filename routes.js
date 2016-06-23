195aae42da7d0c15d80d82a5feb29b9c330b53d7
sagargopale <sagar.gopale@zuneeue.com>
2016-06-23 19:28:27 +0530

    Pushing latest

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     var user = require('./controllers/userController');
     var auth = require('./controllers/authenticationController');
     var customer = require('./controllers/customerController');
+    var invoice = require('./controllers/invoiceController');
 
     //USER MODULE
     app.get('/', function(req, res, next) {
@@ -11,4 +12,5 @@ module.exports = function(app) {
     app.post('/v1/users', user.save);
     app.post('/v1/auth', auth.authenticate);
     app.post('/v1/customers', customer.save);
+    app.post('/v1/invoices', invoice.save);
 };
